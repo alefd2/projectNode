7357a566ca2cf353c0dbacf13a513b4bce43846a
Alef oliveira <alefolivfeira@gmail.com>
2022-09-01 12:00:32 -0300

    feat: Cadastro de conta | validar se tem cpf

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-// medde
-app.use(express.json)
+const costumers = [];
 
 
+app.use(express.json());
 
-app.get("/membros", (request, response) => {
-    return response.json()
+app.post("/account", (request, response)=> {
+    const {cpf, name} = request.body;
+
+    const costumerAreadyExist = costumers.some((costumer)=> costumer.cpf === cpf)
+    
+    if(costumerAreadyExist) {
+        response.status(400).json(
+            {Error:"Costumer already exist" }
+        )
+    }
+
+    costumers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    })
+
+    return response.status(201).send();
+})
+
+app.get("/members", (request, response) => {
+    return response.json(costumers)
 })
 
 
